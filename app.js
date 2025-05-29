33169e2455336c8b9e50e69ffd4cc8e58c0dbe2e
Shivansh Agrawal <64986204+shivanshagrawal46@users.noreply.github.com>
2025-05-30 01:16:52 +0530

    Update app.js

--- app.js
+++ app.js
@@ -251,7 +251,7 @@ app.use('/api', celebrityKundliApiRouter);
 // Learning routes
 app.use('/learning', learningRoutes);
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 }); 
